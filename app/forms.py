b9fa624fcc7c22d8d2aaeee66c338ef0638162d8
Jariel-JonMiller <miller.jarieljon@gmail.com>
2024-04-22 20:38:15 -0500

    added register and new post forms

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,25 @@
 # Add any form classes for Flask-WTF here
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, FileField, EmailField, TextAreaField
 from wtforms.validators import InputRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
+    firstname = StringField('Firstname', validators=[InputRequired()])
+    lastname = StringField('Lastname', validators=[InputRequired()])
+    email = EmailField('Email', validators=[InputRequired()])
+    location = StringField('Location', validators=[InputRequired()])
+    biography = TextAreaField('Biography', validators=[InputRequired()])
+    photo = FileField('Photo', validators=[InputRequired()])
+
+class NewPostForm(FlaskForm):
+    photo = FileField('Photo', validators=[InputRequired()])
+    caption = TextAreaField('Caption', validators=[InputRequired()])
+
+
